5bf5ed13ed4f4b4d9b5fddf05caadc0cf6cdb30c
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-06-13 09:33:07 +0530

    docs - server connected with libaries

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const app = express();
+app.use(express.json());
+
+
+async function connectMongoDB(){
+const conn = await mongoose.connect(process.env.MONGODB_URL);
+if(conn){
+    console.log("Connected to Mongodb");
+}
+}
+connectMongoDB();
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log('listening on port ' + PORT)
+});
